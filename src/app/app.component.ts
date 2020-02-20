bdb17a16d5443de00db7e06b62cfe3774744afd2
Michal Stawicki <stawicki@gmail.com>
2020-02-20 20:01:01 +0100

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sourcemap-bug';
+
+  crashMe(): void {
+    // throw new Error('Bummer');
+    throw new Error($localize`Bummer crash?`);
+  }
+}
